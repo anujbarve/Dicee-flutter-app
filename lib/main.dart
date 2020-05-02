80e918ff34b386166e20521ca9f6e0bd833cb249
Anuj Barve <anujbarve27@gmail.com>
2020-05-02 11:55:21 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.teal,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int ldn = 1;
+  int rdn = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      ldn = Random().nextInt(6) + 1;
+      rdn = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$ldn.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$rdn.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
